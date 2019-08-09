4290b31e73396a5862b7553649d5f8b1cbb60808
Megha Patel <mpatel43@binghamton.edu>
2019-08-09 11:46:47 -0400

    rename screens

--- docs/src/screens/portal.js
+++ docs/src/screens/portal.js
@@ -1,43 +1,6 @@
 
 import React from "react";
 import CreateForm from "../components/create";
-<<<<<<< HEAD
-import "../styles/portal.css";
-
-import { Link, BrowserRouter as Router } from "react-router-dom";
-
-function Portal() {
-  // TEST dummy data
-  let data = [
-    { title: "Hello" },
-    { title: "My" },
-    { title: "Name" },
-    { title: "Jeff" },
-    { title: "Gotti" }
-  ];
-  return (
-    <Router>
-      <h1> My Documents </h1>
-      <div class="portalContainer">
-        <div id="createDoc">
-          <h2> Create a document </h2>
-          <CreateForm label="Create Document: " buttonLabel="Create" />
-        </div>
-        <div>
-          <h2> My Documents </h2>
-          <ul>
-            {data.map(doc => (
-              <li>
-                <Link to={`/${doc.title}`}>{doc.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div id="createDoc">
-          <h2> Add Shared Documents </h2>
-          <CreateForm label="Shared Link: " buttonLabel="Add" />
-        </div>
-=======
 import { Link, BrowserRouter as Router, withRouter } from "react-router-dom";
 
 class Portal extends React.Component {
@@ -94,7 +57,6 @@ class Portal extends React.Component {
         </ul>
         <div> Add Shared Documents </div>
         <CreateForm label="Shared Link: " buttonLabel="Add" />
->>>>>>> e6bd77e01282018560b84a8847fd80607a082f8b
       </div>
     );
   }
