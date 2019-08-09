2e8c1540a74cee2ff15e506023417a5d44c6985b
unknown <gravitymail2723@gmail.com>
2019-08-09 11:19:35 -0400

    fixed damn git problems

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,6 @@ const router = express.Router();
 var { User, Document } = require('../models/models');
 
 router.use(function (req, res, next) {
-  //TESTING
-
   if (!req.user) {
     res.json({
       notAuthenticated: true
@@ -15,24 +13,74 @@ router.use(function (req, res, next) {
   }
 });
 
-router.get("/document/:id", function (req, res) {
-  if (!req.user) {
-    res.json({redirect: "/login"})
+router.get("/documents", async function (req, res) {
+  try {
+    let docs = await Document.find({ owner: req.user._id });
+    console.log("Userdocs FETCH: ", docs)
+    res.json(docs);
+  } catch{
+    console.log("Error fetching user")
   }
 })
 
-router.post("/document/:id", function (req, res) {
-  new Document({
-    owner: req.user._id,
-    editorState: {
-      myState: req.body.editorState
+router.get("/document/:id", async function (req, res) {
+  try {
+    let doc = await Document.findById(req.params.id);
+    if (!doc) {
+      throw "Error fetching doc"
     }
-  }).save(function (err, doc) {
+    res.json(doc);
+  } catch{
+    console.log("Error fetching doc")
+    res.json({ redirect: "/portal" })
+  }
+})
+
+// Return success true and id of new doc
+router.post("/newDoc", async function (req, res) {
+  let newDoc = {
+    owner: req.user._id,
+    content: req.body.content,
+  }
+  if (req.body.title) {
+    newDoc.title = req.body.title;
+  }
+  new Document(newDoc).save(function (err, doc) {
     if (err) res.json({ error: err })
     else {
-      res.json({ success: true })
+      res.json({ success: true, id: doc._id })
     }
   });
+})
+
+// Save document
+router.post("/document", async function (req, res) {
+  const doc = await Document.findById(req.body.id);
+
+  if (!doc) {
+    let newDoc = {
+      owner: req.user._id,
+      content: req.body.content,
+      lastUpdated: new Date()
+
+    }
+    if (req.body.title) {
+      newDoc.title = req.body.title;
+    }
+    new Document(newDoc).save(function (err, doc) {
+      if (err) res.json({ error: err })
+      else {
+        res.json({ success: true })
+      }
+    });
+  }
+  else {
+    doc.content = req.body.content;
+    doc.lastUpdated = new Date();
+    if (req.body.title)
+      doc.title = req.body.title;
+    await doc.save();
+  }
 })
 
 module.exports = router;
