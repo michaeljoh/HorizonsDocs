4eaaaab4131be08a54ac4f2cb3c34090f229695c
unknown <gravitymail2723@gmail.com>
2019-08-09 10:50:56 -0400

    add everything back

---
+++ server/app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var models = require('./models/models');
+var routes = require('./routes/index');
+var auth = require('./routes/auth');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+var _ = require('lodash');
+const cors = require("cors")
+
+var app = express();
+
+app.use(cors());
+
+// view engine setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Session info here
+app.use(session({
+  secret: process.env.SECRET,
+  name: "CookieMon",
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Passport Serialize
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+})
+// Passport Deserialize
+passport.deserializeUser(function (id, done) {
+  models.User.findById(id, function (err, user) {
+    done(err, user);
+  })
+})
+
+// Passport Strategy
+passport.use(new LocalStrategy({ 
+  usernameField: 'email',    // define the parameter in req.body that passport can use as username and password
+  passwordField: 'password'
+}, function (email, password, done) {
+  console.log("in strategy")
+  models.User.findOne({ email: email }, function (err, user) {
+    if (err) {
+      console.log(err);
+      done(err);
+    }
+    else if (!user) {
+      console.log("NO USER FOUND")
+      done(null, false, { message: "incorrect email" });
+    }
+    else if (user.password !== password) {
+      console.log("PASSWORDS DONT MATCH")
+
+      done(null, false, { message: "incorrect password" });
+    }
+    else {
+      done(null, user);
+    }
+  })
+}));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/', auth(passport));
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
---
+++ server/routes/auth.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+const { User } = require('../models/models.js');
+
+module.exports = function (passport) {
+  // Add Passport-related auth routes here, to the router!
+
+  // POST Login page
+  router.post(
+    '/login',
+    function (req, res, next) {
+      passport.authenticate('local', function (err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.json({ redirect: '/login' }); }
+
+        req.logIn(user, function (err) {
+          if (err) { return next(err); }
+          return res.json({ redirect: '/portal' });
+        });
+      })(req, res, next)
+    }
+  );
+
+  // Add unique email check
+  const validateReq = async function (userData) {
+    if (!userData.email) {
+      return 'Please enter a username.';
+    }
+    if (!userData.password) {
+      return 'Please enter a password.';
+    }
+
+    const user = await User.findOne({email: userData.email});
+    if (user)
+      return "User already exists";
+  };
+
+  // POST registration 
+  router.post('/signup', async function (req, res) {
+    // validation step
+    let error = await validateReq(req.body);
+    if (error) {
+      return res.send(`Signup error: ${error}`);
+    }
+
+    let u = new User({
+      email: req.body.email,
+      password: req.body.password,
+      name: {
+        first: req.body.name.first,
+        last: req.body.name.last
+      }
+    });
+
+    u.save(function (err, user) {
+      if (err) {
+        res.send('Error signing up');
+      }
+      console.log('Saved User: ', user);
+      res.send('User successfully created');
+    });
+  });
+
+  router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/login');
+  });
+
+  return router;
+};
